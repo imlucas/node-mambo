7d0b06629a36fa2c349ce7aa3c5b920387d724a3
Jonathan Marmor <jmarmor@gmail.com>
2012-10-13 11:54:02 -0400

    Assume tables exist.

--- index.js
+++ index.js
@@ -25,155 +25,6 @@ function Model(tableData){
     this.tableData = tableData;
 }
 
-Model.prototype.ensureTable = function(tableData){
-    var d = when.defer();
-    sequence(this).then(function(next){
-        // Does the table already exist in DynamoDB?
-        this.db.describeTable({'TableName': tableData.tableName}, next);
-    }).then(function(next, err, description){
-        if(err){
-            return next(err);
-        }
-        else if(description.CreationDateTime !== 0){
-            // Table already exists
-            log.debug(tableData.tableName + " already exists");
-            return d.resolve(true);
-        }
-        else {
-            log.error("Problem getting " + tableData.tableName + " description: " + description);
-            return d.resolve(false);
-        }
-    }).then(function(next, err){
-        if(err.name.indexOf("ResourceNotFound") !== -1){
-            log.debug(tableData.tableName + " doesn't exist yet, so create it.");
-            // Table doesn't exist yet, so create it.
-            return next();
-        }
-        log.error("Error getting " + tableData.tableName + " description: " + err);
-        return d.resolve(false);
-    }).then(function(next){
-        // Create the keySchema data
-        var keySchema = {
-            'HashKeyElement': {
-                'AttributeName': tableData.hashName,
-                'AttributeType': tableData.hashType
-            }
-        };
-        if(tableData.rangeName){
-            keySchema.RangeKeyElement = {
-                'AttributeName': tableData.rangeName,
-                'AttributeType': tableData.rangeType
-            };
-        }
-        next(keySchema);
-    }).then(function(next, keySchema){
-        // Create the table in DynamoDB
-        this.db.createTable({
-            'TableName': tableData.tableName,
-            'ProvisionedThroughput': {
-                'ReadCapacityUnits': tableData.read,
-                'WriteCapacityUnits': tableData.write
-            },
-            'KeySchema': keySchema
-        }, next);
-    }).then(function(next, err, table){
-        if(!d.rejectIfError(err)){
-            log.info(tableData.tableName + " created in DynamoDB: " + JSON.stringify(table));
-            return d.resolve(true);
-        }
-        if(err.name.indexOf("ThrottlingException") !== -1){
-            log.warn("Got ThrottlingException from AWS DynamoDB when creating" + tableData.tableName);
-            return d.resolve(false);
-        }
-        log.debug("Something unexpected happened when creating: " + tableData.tableName + ": " + err);
-    });
-    return d.promise;
-};
-
-Model.prototype.ensureAllTables = function(status){
-    when.all(this.tableData.map(function(t){
-        var d = when.defer(),
-            tableName = (this.prefix || "") + t.table;
-        t.tableName = tableName;
-
-        log.debug("status for " + t.tableName + " " + status[t.tableName]);
-
-        if(status[t.tableName] !== 'done'){
-            sequence(this).then(function(next){
-            //     log.info("delaying 2000ms in outer loop");
-            //     setTimeout(next, 2000);
-            // }).then(function(next){
-                this.ensureTable(t).then(function(success){
-                    console.log("Did ensureTable succeed for " + t.tableName + "?:" + success);
-                    next(success);
-                });
-            }).then(function(next, success){
-                log.debug("ensureTable " + t.tableName);
-                if(success){
-                    log.debug(t.tableName + " succeeded");
-                    next();
-                }
-                else {
-                    log.error(t.tableName + " was not created.");
-                    status[t.tableName] = 'failed';
-                    return d.resolve();
-                }
-            }).then(function(next){
-                status[t.tableName] = 'done';
-                log.debug("status[" + t.tableName + "]: " + status[t.tableName]);
-                this.tables[t.alias] = this.db.get(t.tableName);
-
-                // @todo is the following necessary?
-                this.tables[t.alias].name = this.tables[t.alias].TableName;
-                this.tables[t.alias].girth = {
-                    'read': t.read,
-                    'write': t.write
-                };
-
-                // Parse table hash and range names and types defined in package.json
-                var localSchema = {},
-                    typeMap = {
-                        'N': Number,
-                        'Number': Number,
-                        'NS': [Number],
-                        'NumberSet': [Number],
-                        'S': String,
-                        'String': String,
-                        'SS': [String],
-                        'StringSet': [String]
-                    };
-                localSchema[t.hashName] = typeMap[t.hashType];
-                if (t.rangeName){
-                    localSchema[t.rangeName] = typeMap[t.rangeType];
-                }
-
-                this.tables[t.alias].schema = localSchema;
-
-                this.girths[t.alias] = {
-                    'read': t.read,
-                    'write': t.write
-                };
-                return d.resolve();
-            });
-        }
-        return d.promise;
-    }.bind(this)), function(){
-        var tryAgain = false;
-        this.tableData.forEach(function(t){
-            if(status[t.tableName] !== 'done'){
-                tryAgain = true;
-            }
-        });
-        if(tryAgain){
-            log.warn("Running ensureAllTables again with status: " + JSON.stringify(status));
-            this.ensureAllTables(status);
-        }
-        else {
-            log.debug("Not running ensureAllTables again: " + JSON.stringify(status));
-        }
-    }.bind(this));
-};
-
 Model.prototype.connect = function(key, secret, prefix, region){
     this.prefix = prefix;
     this.region = region;
@@ -186,54 +37,50 @@ Model.prototype.connect = function(key, secret, prefix, region){
             'secretAccessKey': secret
         });
         this.db = this.client.get(this.region || "us-east-1");
-
-        this.ensureAllTables({});
     } else {
         // Connect to Magneto
         log.info("Connecting to Magneto");
         this.client = dynamo.createClient();
         this.client.useSession = false;
-
         this.db = this.client.get(this.region || "us-east-1");
-
         this.db.host = "localhost";
         this.db.port = 8081;
-
-        this.tableData.forEach(function(table){
-            var schema = {},
-                typeMap = {
-                    'N': Number,
-                    'Number': Number,
-                    'NS': [Number],
-                    'NumberSet': [Number],
-                    'S': String,
-                    'String': String,
-                    'SS': [String],
-                    'StringSet': [String]
-                },
-                tableName = (this.prefix || "") + table.table;
-
-            this.tables[table.alias] = this.db.get(tableName);
-            this.tables[table.alias].name = this.tables[table.alias].TableName;
-            this.tables[table.alias].girth = {
-                'read': table.read,
-                'write': table.write
-            };
-
-            // Parse table hash and range names and types defined in package.json
-            schema[table.hashName] = typeMap[table.hashType];
-            if (table.rangeName){
-                schema[table.rangeName] = typeMap[table.rangeType];
-            }
-
-            this.tables[table.alias].schema = schema;
-
-            this.girths[table.alias] = {
-                'read': table.read,
-                'write': table.write
-            };
-        }.bind(this));
     }
+
+    this.tableData.forEach(function(table){
+        var schema = {},
+            typeMap = {
+                'N': Number,
+                'Number': Number,
+                'NS': [Number],
+                'NumberSet': [Number],
+                'S': String,
+                'String': String,
+                'SS': [String],
+                'StringSet': [String]
+            },
+            tableName = (this.prefix || "") + table.table;
+
+        this.tables[table.alias] = this.db.get(tableName);
+        this.tables[table.alias].name = this.tables[table.alias].TableName;
+        this.tables[table.alias].girth = {
+            'read': table.read,
+            'write': table.write
+        };
+
+        // Parse table hash and range names and types defined in package.json
+        schema[table.hashName] = typeMap[table.hashType];
+        if (table.rangeName){
+            schema[table.rangeName] = typeMap[table.rangeType];
+        }
+
+        this.tables[table.alias].schema = schema;
+
+        this.girths[table.alias] = {
+            'read': table.read,
+            'write': table.write
+        };
+    }.bind(this));
 
     this.connected = true;
     return this;
