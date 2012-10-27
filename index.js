207b818db953ce7f17c28b52d0046e77447b04c5
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-27 18:38:04 -0400

    Updating docs.  Added BooleanField

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var dynamo = require("dynamo"),
     sequence = require("sequence"),
     winston = require("winston"),
     _ = require("underscore"),
-    Query = require('./lib/query');
+    Query = require('./lib/query'),
+    fields = require('./lib/fields');
 
 // Setup logger
-winston.loggers.add("app", {
+var log = winston.loggers.add("mambo", {
     console: {
         'level': "silly",
-        'timestamp': true,
+        'timestamp': false,
         'colorize': true
     }
 });
-var log = winston.loggers.get("app");
 
 
 // Models have many tables.
@@ -117,10 +117,12 @@ Model.prototype.connect = function(key, secret, prefix, region){
     return this;
 };
 
+// Get a dynamo table object by alias.
 Model.prototype.table = function(alias){
     return this.tables[alias];
 };
 
+// Create all tables as defined by this models schemas.
 Model.prototype.createAll = function(){
     var d = when.defer();
     when.all(Object.keys(this.tables).map(this.ensureTableMagneto.bind(this)), d.resolve);
@@ -152,23 +154,21 @@ Model.prototype.ensureTableMagneto = function(alias){
     return d.promise;
 };
 
+// Fetch a query wrapper Django style.
 Model.prototype.objects = function(alias, hash, range){
     return new Query(this, alias, hash, range);
 };
 
+// Low level get item wrapper.
+// Params:
+// - alias: The table alias name
+// - hash: the value of the key-hash of the object you want to retrieve, eg:
+// - the song ID
+// - range: the value of the key-range of the object you want to retrieve
+// - attributesToGet: An array of names of attributes to return in each
+// - object. If empty, get all attributes.
+// - consistentRead: boolean
 Model.prototype.get = function(alias, hash, range, attributesToGet, consistentRead){
-    // alias: The table alias name
-
-    // hash: the value of the key-hash of the object you want to retrieve, eg:
-    // the song ID
-
-    // range: the value of the key-range of the object you want to retrieve
-
-    // attributesToGet: An array of names of attributes to return in each
-    // object. If empty, get all attributes.
-
-    // consistentRead: boolean
-
     var d = when.defer(),
         table = this.table(alias),
         request;
@@ -207,19 +207,19 @@ Model.prototype.get = function(alias, hash, range, attributesToGet, consistentRe
     return d.promise;
 };
 
+// Lowlevel delete item wrapper
+// example:
+//     delete('alias', 'hash', {
+//          'range': 'blahblah',
+//          'expectedValues': [{
+//              'attributeName': 'attribute_name',
+//              'expectedValue': 'current_value', // optional
+//              'exists': 'true' // defaults to true
+//            }],
+//          'returnValues':  'NONE'
+//        })
+
 Model.prototype.delete = function(alias, hash, deleteOpts){
-
-    // usage:
-    // delete('alias', 'hash', {
-    //      'range': 'blahblah',
-    //      'expectedValues': [{
-    //          'attributeName': 'attribute_name',
-    //          'expectedValue': 'current_value', // optional
-    //          'exists': 'true' // defaults to true
-    //        }],
-    //      'returnValues':  'NONE'
-    //    })
-
     var d = when.defer(),
         table = this.table(alias),
         deleteRequest = {},
@@ -280,39 +280,38 @@ Model.prototype.deleteAllItems = function() {
 
 };
 
+// Accepts an array of objects
+// Each object should look like this:
+// {
+//     'alias': 'url',
+//     'hashes': [2134, 1234],
+//     'ranges': [333333, 222222],
+//     'attributesToGet': ['url']
+// }
+// alias is the table alias name
+// hashes is an array of key-hashes of objects you want to get from this table
+// ranges is an array of key-ranges of objects you want to get from this table
+// only use ranges if this table has ranges in its key schema
+// hashes and ranges must be the same length and have corresponding values
+// attributesToGet is an array of the attributes you want returned for each
+// object. Omit if you want the whole object.
+
+// Example:
+// To get the urls of songs 1, 2, and 3 and the entire love objects for
+// love 98 with created value 1350490700640 and love 99 with 1350490700650:
+// [
+//     {
+//         'alias': 'song',
+//         'hashes': [1, 2, 3],
+//         'attributesToGet': ['url']
+//     },
+//     {
+//         'alias': 'loves',
+//         'hashes': [98, 99],
+//         'ranges': [1350490700640, 1350490700650]
+//     },
+// ]
 Model.prototype.batchGet = function(req){
-    // Accepts an array of objects
-    // Each object should look like this:
-    // {
-    //     'alias': 'url',
-    //     'hashes': [2134, 1234],
-    //     'ranges': [333333, 222222],
-    //     'attributesToGet': ['url']
-    // }
-    // alias is the table alias name
-    // hashes is an array of key-hashes of objects you want to get from this table
-    // ranges is an array of key-ranges of objects you want to get from this table
-    // only use ranges if this table has ranges in its key schema
-    // hashes and ranges must be the same length and have corresponding values
-    // attributesToGet is an array of the attributes you want returned for each
-    // object. Omit if you want the whole object.
-
-    // Example:
-    // To get the urls of songs 1, 2, and 3 and the entire love objects for
-    // love 98 with created value 1350490700640 and love 99 with 1350490700650:
-    // [
-    //     {
-    //         'alias': 'song',
-    //         'hashes': [1, 2, 3],
-    //         'attributesToGet': ['url']
-    //     },
-    //     {
-    //         'alias': 'loves',
-    //         'hashes': [98, 99],
-    //         'ranges': [1350490700640, 1350490700650]
-    //     },
-    // ]
-
     var d = when.defer(),
         request,
         results = [],
@@ -370,7 +369,7 @@ Model.prototype.batchGet = function(req){
     return d.promise;
 };
 
-var accept = function(item){
+var isFalsy = function(item){
     // Returns true if item is not undefined, null, "", [], or {}.
     if(item === false){return true;}
     if(item === 0){return true;}
@@ -381,6 +380,7 @@ var accept = function(item){
     return true;
 };
 
+// @todo (lucas) This shouldnt be in here.
 Model.prototype.checkForBase36 = function(id){
     if (typeof id === 'string'){
         var idString = id.split(''),
@@ -424,6 +424,7 @@ Model.prototype.sortObjects = function(objects, values, property){
     });
 };
 
+// @todo (lucas) Remove this. Deprecated by schema.
 Model.prototype.valueToDynamo = function(value, dynamoType, exfmType){
     var newValue;
 
@@ -449,12 +450,13 @@ Model.prototype.valueToDynamo = function(value, dynamoType, exfmType){
     return value;
 };
 
+// @todo (lucas) Remove this. Deprecated by schema.
 Model.prototype.toDynamo = function(alias, obj){
     var table = this.table(alias),
         dynamoObj = {'TableName': table.name, 'Item': {}};
 
     Object.keys(obj).map(function(attr){
-        if(accept(obj[attr])){
+        if(isFalsy(obj[attr])){
             var dynamoType = table.attributeSchema[attr].dynamoType,
                 exfmType = table.attributeSchema[attr].exfmType,
                 value = obj[attr];
@@ -467,6 +469,7 @@ Model.prototype.toDynamo = function(alias, obj){
     return dynamoObj;
 };
 
+// @todo (lucas) Remove this. Deprecated by schema.
 Model.prototype.valueFromDynamo = function(value, dynamoType, exfmType){
     var newValue;
     if(dynamoType === "N"){
@@ -494,6 +497,7 @@ Model.prototype.valueFromDynamo = function(value, dynamoType, exfmType){
     return value;
 };
 
+// @todo (lucas) Remove this. Deprecated by schema.
 Model.prototype.fromDynamo = function(alias, dynamoObj){
     var obj = {};
     Object.keys(dynamoObj).map(function(attr){
@@ -510,38 +514,35 @@ Model.prototype.fromDynamo = function(alias, dynamoObj){
     return obj;
 };
 
+// http://docs.amazonwebservices.com/amazondynamodb/latest/developerguide/API_PutItem.html
+
+// alias: The table alias name
+
+// obj: The object to put in the table. This method will handle formatting
+// the object and casting.
+// Sample:
+// {
+//     "url":"http://thissongexistsforreallzz.com/song1.mp3",
+//     "id":30326673248,
+//     "url_md5":"66496db3a1bbba45fb189030954e78d0",
+//     "metadata_state":"pending",
+//     "loved_count":0,
+//     "listened":0,
+//     "version":1,
+//     "created":1350500174375
+// }
+//
+
+// expected: See AWS docs for an explanation. This method handles casting
+// and supplying the attribute types, so this object is somewhat simplified
+// from what AWS accepts.
+// Sample:
+// {
+//     'metadata_state': {'Value': 'pending', 'Exists': true},
+//     'version': {'Value': 0, 'Exists': true}
+// }
+// returnValues: See AWS docs for an explanation.
 Model.prototype.put = function(alias, obj, expected, returnOldValues){
-
-    // http://docs.amazonwebservices.com/amazondynamodb/latest/developerguide/API_PutItem.html
-
-    // alias: The table alias name
-
-    // obj: The object to put in the table. This method will handle formatting
-    // the object and casting.
-    // Sample:
-    // {
-    //     "url":"http://thissongexistsforreallzz.com/song1.mp3",
-    //     "id":30326673248,
-    //     "url_md5":"66496db3a1bbba45fb189030954e78d0",
-    //     "metadata_state":"pending",
-    //     "loved_count":0,
-    //     "listened":0,
-    //     "version":1,
-    //     "created":1350500174375
-    // }
-    //
-
-    // expected: See AWS docs for an explanation. This method handles casting
-    // and supplying the attribute types, so this object is somewhat simplified
-    // from what AWS accepts.
-    // Sample:
-    // {
-    //     'metadata_state': {'Value': 'pending', 'Exists': true},
-    //     'version': {'Value': 0, 'Exists': true}
-    // }
-
-    // returnValues: See AWS docs for an explanation.
-
     var d = when.defer(),
         table = this.table(alias),
         request;
@@ -605,8 +606,6 @@ Model.prototype.updateItem = function(alias, hash, attrs, updateOpts){
         opts = {};
 
     table = this.table(alias);
-    // updateRequest[table.name] = {};
-
     if (updateOpts) {
         opts = updateOpts;
     }
@@ -671,27 +670,25 @@ Model.prototype.updateItem = function(alias, hash, attrs, updateOpts){
     return d.promise;
 };
 
+// usage:
+// query('alias', 'hash', {
+//     'limit': 2,
+//     'consistentRead': true,
+//     'scanIndexForward': true,
+//     'rangeKeyCondition': {
+//         'attributeValueList': [{
+//             'attributeName': 'blhah',
+//             'attributeValue': 'some_value'
+//         }],
+//         'comparisonOperator': 'GT'
+//     },
+//     'exclusiveStartKey': {
+//         'hashName': 'some_hash',
+//         'rangeName': 'some_range'
+//     },
+//     'attributeToGet':  ['attribute']
+// })
 Model.prototype.query = function(alias, hash, queryOpts){
-
-        // usage:
-        // query('alias', 'hash', {
-        //     'limit': 2,
-        //     'consistentRead': true,
-        //     'scanIndexForward': true,
-        //     'rangeKeyCondition': {
-        //         'attributeValueList': [{
-        //             'attributeName': 'blhah',
-        //             'attributeValue': 'some_value'
-        //         }],
-        //         'comparisonOperator': 'GT'
-        //     },
-        //     'exclusiveStartKey': {
-        //         'hashName': 'some_hash',
-        //         'rangeName': 'some_range'
-        //     },
-        //     'attributeToGet':  ['attribute']
-        // })
-
     var d = when.defer(),
         queryRequest = {},
         response = [],
@@ -731,8 +728,6 @@ Model.prototype.query = function(alias, hash, queryOpts){
             attributeValue[dynamoType] = this.valueToDynamo(
                 attr.attributeValue, dynamoType);
             attributeValueList.push(attributeValue);
-            // rangeKey[table.rangeType] = attr.attributeValue.toString();
-            // updateRequest.Key.RangeKeyElement = rangeKey;
         });
         queryRequest.RangeKeyCondition = {
             'AttributeValueList': attributeValueList,
@@ -782,65 +777,6 @@ Model.prototype.query = function(alias, hash, queryOpts){
     }.bind(this));
     return d.promise;
 };
-
-// Model.prototype.deleteAllItems = function(alias) {
-//     var d = when.defer(),
-//         table = this.table(alias),
-//         scanRequest = {
-//             'TableName': table.name
-//         },
-//         deleteRequest = {
-//             'RequestItems': {}
-//         };
-
-//     sequence(this).then(function(next){
-//         this.db.scan(scanRequest, function(err, data){
-//             if(!err){
-//                 next(data.Items);
-//             }
-//             else{
-//                 throw new Error(err);
-//             }
-//         });
-//     }).then(function(next, scanResults){
-
-//         var seperatedRequests = [],
-//             i,
-//             j,
-//             chunkSize = 25;
-
-//         // seperate the scan results into chunks
-//         for (i = 0, j = scanResults.length; i < j; i += chunkSize) {
-//             seperatedRequests.push(scanResults.slice(i, i + chunkSize));
-//         }
-
-
-//         when.all(seperatedRequests.map(function(requests){
-//             var p = when.defer(),
-//                 deleteRequests = [];
-//             requests.forEach(function(item){
-//                 deleteRequests.push({
-//                     'DeleteRequest': {
-//                         'Key': {
-//                             'HashKeyElement': item[table.hashName]
-//                         }
-//                     }
-//                 });
-//             });
-//             deleteRequest.RequestItems[table.name] = deleteRequests;
-//             this.db.batchWriteItem(deleteRequest, function(err, data){
-//                 p.resolve(true);
-//             });
-//             return p.promise;
-//         }.bind(this)), function(){
-//             d.resolve(true);
-//         });
-
-//         d.resolve();
-//     });
-
-//     return d.promise;
-// };
 
 //                     ##      ##    ###    ########  ##    ## #### ##    ##  ######
 //  ##   ##   ##   ##  ##  ##  ##   ## ##   ##     ## ###   ##  ##  ###   ## ##    ##   ##   ##   ##   ##
@@ -952,8 +888,9 @@ Model.prototype.isTableActive = function(tableName){
     return d.promise;
 };
 
-Model.prototype.batchWriteItem = function(alias){
-    // this.db.batchWriteItem(request, function(err, data){
-};
-
 module.exports = Model;
+module.exports.StringField = fields.StringField;
+module.exports.NumberField = fields.NumberField;
+module.exports.JSONField = fields.JSONField;
+module.exports.DateField = fields.DateField;
+module.exports.BooleanField = fields.BooleanField;
