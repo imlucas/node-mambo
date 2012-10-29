69d052c48ef1a89d3f8fdf39818122de26c35ace
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-28 21:53:25 -0400

    prototyping some new higher level api's in tests first before making them actually work.  Need to see how they feel...

--- test/index.test.js
+++ test/index.test.js
@@ -21,12 +21,78 @@ var songSchema = new Schema('Song', 'song', 'id', {
     'no_ones': NumberSetField
 });
 
-var Song = new mambo.Model(songSchema);
+var loveSchema = new Schema('SongLoves', 'loves', ['id', 'created'], {
+    'id': NumberField,
+    'username': StringField,
+    'created': NumberField
+});
+
+var Song = new mambo.Model(songSchema, loveSchema);
 
 describe('Model', function(){
     it('should have all the schemas available after connecting', function(){
         Song.connect();
+
         assert.ok(Song.schema('song'), 'Should have song schema');
         assert.ok(Song.table('song'), 'Should have song table');
+    });
+
+    it('should handle basic puts', function(done){
+        Song.insert('song').set({
+            'id': 1,
+            'title': 'Silence in a Sweater',
+            'created': new Date(),
+            'recent_loves': [{
+                'username': 'lucas'
+            }],
+            'tags': [
+                'silence',
+                'sweaters'
+            ],
+            'no_ones': [
+                Date.now()
+            ]
+        }).commit().then(function(){
+            done();
+        });
+    });
+
+    it('should handle batch inserts', function(done){
+        Song.insert('song').set({
+            'id': 1
+        }).insert('song').set({
+            'id': 2
+        }).commit().then(function(){
+            done();
+        });
+    });
+
+    it('should automatically split batches that are two large', function(done){
+        var q = Song.insert('song').set({
+            'id': 1
+        }), i = 2;
+        for(i; i <= 50; i++){
+            q.insert('song').set({
+                'id': i
+            });
+        }
+
+        q.commit().then(function(songs){
+            assert.equal(songs.length, 50);
+            done();
+        });
+    });
+    it('should handle batch inserts across tables', function(done){
+        Song.insert('song').set({
+            'id': 1
+        }).insert('loves').set({
+            'id': 1,
+            'username': 'lucas',
+            'created': new Date()
+        }).commit().then(function(docs){
+            assert.equal(docs.loves.length, 1);
+            assert.equal(docs.songs.length, 1);
+            done();
+        });
     });
 });
