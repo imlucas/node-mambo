e6b86f896fb267b888228c84d19ac9ea2f726ad8
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-12-09 21:59:22 -0500

    First pass of scan.  Now to add to magneto.

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ Model.prototype.batchWrite = function(puts, deletes){
     }
 
     log.silly('Built BATCH_WRITE request: ' + util.inspect(req, false, 10));
-    return this.db.batchWriteItem(req, function(err, data){
+    return this.db.batchWriteItem(req).then(function(data){
         log.silly('BATCH_WRITE returned: ' + util.inspect(data, false, 5));
         var success = {};
         Object.keys(data.Responses).forEach(function(tableName){
@@ -569,7 +569,7 @@ Model.prototype.updateItem = function(alias, hash, attrs, opts){
     log.silly('Built UPDATE_ITEM request: ' + util.inspect(request, false, 10));
 
     // Make the request
-    return this.db.updateItem(request, function(err, data){
+    return this.db.updateItem(request).then(function(data){
         log.silly('UPDATE_ITEM returned: ' + util.inspect(data, false, 5));
         if (opts.returnValues !== undefined) {
             return schema.import(data.Attributes);
@@ -671,13 +671,170 @@ Model.prototype.query = function(alias, hash, opts){
     log.silly('Built QUERY request: ' + util.inspect(request, false, 10));
 
     // Make the request
-    return this.db.query(request, function(err, data){
+    return this.db.query(request).then(function(data){
         log.silly('QUERY returned: ' + util.inspect(data, false, 5));
         return data.Items.map(function(item){
             return schema.import(item);
         });
     }, function(err){
         log.error('QUERY: ' + err.message + '\n' + err.stack);
+        return err;
+    });
+};
+
+
+// this.scan('song', {
+//         'available': {'EQ': '-1'},
+//         'loves': {'NE': 0},
+//         'title': 'NOT_NULL',
+//         'artist': 'NULL',
+//     },
+//     {
+//         'limit': 10,
+//         'startKey': {
+//             'id': 100
+//         },
+//         'fields': ['id', 'title']
+//     });
+var opToArgLength = {
+    'EQ': 1,
+    'NE': 1,
+    'LE': 1,
+    'LT': 1,
+    'GE': 1,
+    'GT': 1,
+    'NOT_NULL': 0,
+    'NULL': 0,
+    'CONTAINS': 1,
+    'NOT_CONTAINS': 1,
+    'BEGINS_WITH': 1,
+    'IN': null,
+    'BETWEEN': 2
+};
+
+function Filter(schema, key, data){
+    this.schema = schema;
+    this.field = schema.field(key);
+    this.operator = undefined;
+    this.value = [];
+
+    if(data === new Object(data)){
+        this.operator = Object.keys(data)[0];
+        this.value = data[this.operator];
+        if(!Array.isArray(this.value)){
+            this.value = [this.value];
+        }
+    }
+    else {
+        this.operator = data;
+        // @todo (lucas) Assert its actually a valueles condition.
+    }
+    var expectLen = opToArgLength[this.operator];
+    if(expectLen !== null && this.value.length !== expectLen){
+        throw new Error('Invalid number of args for ' +
+            key + ' filter: ' +this.operator + ' ' + this.value);
+    }
+}
+
+Filter.prototype.export = function(){
+    var ret = {
+        'AttributeValueList': [],
+        'ComparisonOperator': 'EQ'
+    };
+    if(this.value){
+        ret.AttributeValueList = this.value.map(function(val){
+            var ret = {};
+            ret[this.field.shortType] = this.field.export(val);
+            return ret;
+        }.bind(this));
+    }
+    return ret;
+};
+
+function ScanResult(model, alias, data){
+    var self = this,
+        hashField,
+        rangeField;
+
+    this.model = model;
+    this.schema = this.model.schema(alias);
+    this.count = data.Count;
+    this.items = data.Items.map(function(item){
+        return self.schema.import(item);
+    });
+    this.consumedCapacityUnits = data.ConsumedCapacityUnits;
+    this.scannedCount = data.ScannedCount;
+    this.isLastPage = data.LastEvaluatedKey === null;
+    this.lastEvaluatedKey = (this.isLastPage) ? null : {};
+
+    if(!this.isLastPage){
+        hashField = this.schema.field(this.schema.hash);
+        this.lastEvaluatedKey[hashField.name] = hashField.import(
+            data.LastKeyEvaluated.HashKeyElement[hashField.shorType]
+        );
+
+        if(this.schema.range){
+            rangeField = this.schema.field(this.schema.range);
+            this.lastEvaluatedKey[rangeField.name] = rangeField.import(
+                data.LastKeyEvaluated.RangeKeyElement[rangeField.shorType]
+            );
+        }
+    }
+}
+
+// @todo (lucas) Get next result.
+ScanResult.prototype.next = function(){
+
+};
+
+// @todo (lucas) Get ALL results.
+ScanResult.prototype.all = function(){
+
+};
+
+
+
+Model.prototype.scan = function(alias, filter, opts){
+    var self = this,
+        schema = this.schema(alias),
+        req = {
+            'TableName': schema.tableName,
+            'ScanFilter': {}
+        };
+
+    if(opts.limit !== undefined){
+        req.Limit = opts.limit;
+    }
+
+    if(opts.startKey !== undefined){
+        req.ExclusiveStartKey = schema.exportKey(opts.startKey);
+    }
+
+    if(opts.count !== undefined && opts.fields !== undefined){
+        throw new Error('Can\'t specify count and fields in the same scan.');
+    }
+
+    if(opts.count !== undefined){
+        req.Count = opts.count;
+    }
+
+    if(opts.fields !== undefined){
+        req.AttributesToGet = opts.fields;
+    }
+
+    Object.keys(filter).forEach(function(key){
+        var f = new Filter(schema, key, filter[key]);
+        req.ScanFilter[key] = f.export();
+
+    });
+    log.silly('Built SCAN request: ' + util.inspect(req, false, 10));
+
+    // Make the request
+    return this.db.scan(req).then(function(data){
+        log.silly('SCAN returned: ' + util.inspect(data, false, 5));
+        return new ScanResult(self, alias, data);
+    }, function(err){
+        log.error('SCAN: ' + err.message + '\n' + err.stack);
         return err;
     });
 };
