ea5ba8ce00139d9534546608e80e84aac638792d
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-12-09 22:35:58 -0500

    More work on scan.  Now has a nice fluent interface.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var aws = require("plata"),
     Schema = require('./lib/schema'),
     fields = require('./lib/fields'),
     Inserter = require('./lib/inserter'),
+    Scanner = require('./lib/scan'),
     util = require('util'),
     EventEmitter = require('events').EventEmitter,
     plog = require('plog'),
@@ -682,119 +683,12 @@ Model.prototype.query = function(alias, hash, opts){
     });
 };
 
-
-// this.scan('song', {
-//         'available': {'EQ': '-1'},
-//         'loves': {'NE': 0},
-//         'title': 'NOT_NULL',
-//         'artist': 'NULL',
-//     },
-//     {
-//         'limit': 10,
-//         'startKey': {
-//             'id': 100
-//         },
-//         'fields': ['id', 'title']
-//     });
-var opToArgLength = {
-    'EQ': 1,
-    'NE': 1,
-    'LE': 1,
-    'LT': 1,
-    'GE': 1,
-    'GT': 1,
-    'NOT_NULL': 0,
-    'NULL': 0,
-    'CONTAINS': 1,
-    'NOT_CONTAINS': 1,
-    'BEGINS_WITH': 1,
-    'IN': null,
-    'BETWEEN': 2
-};
-
-function Filter(schema, key, data){
-    this.schema = schema;
-    this.field = schema.field(key);
-    this.operator = undefined;
-    this.value = [];
-
-    if(data === new Object(data)){
-        this.operator = Object.keys(data)[0];
-        this.value = data[this.operator];
-        if(!Array.isArray(this.value)){
-            this.value = [this.value];
-        }
-    }
-    else {
-        this.operator = data;
-        // @todo (lucas) Assert its actually a valueles condition.
-    }
-    var expectLen = opToArgLength[this.operator];
-    if(expectLen !== null && this.value.length !== expectLen){
-        throw new Error('Invalid number of args for ' +
-            key + ' filter: ' +this.operator + ' ' + this.value);
-    }
-}
-
-Filter.prototype.export = function(){
-    var ret = {
-        'AttributeValueList': [],
-        'ComparisonOperator': 'EQ'
-    };
-    if(this.value){
-        ret.AttributeValueList = this.value.map(function(val){
-            var ret = {};
-            ret[this.field.shortType] = this.field.export(val);
-            return ret;
-        }.bind(this));
-    }
-    return ret;
-};
-
-function ScanResult(model, alias, data){
-    var self = this,
-        hashField,
-        rangeField;
-
-    this.model = model;
-    this.schema = this.model.schema(alias);
-    this.count = data.Count;
-    this.items = data.Items.map(function(item){
-        return self.schema.import(item);
-    });
-    this.consumedCapacityUnits = data.ConsumedCapacityUnits;
-    this.scannedCount = data.ScannedCount;
-    this.isLastPage = data.LastEvaluatedKey === null;
-    this.lastEvaluatedKey = (this.isLastPage) ? null : {};
-
-    if(!this.isLastPage){
-        hashField = this.schema.field(this.schema.hash);
-        this.lastEvaluatedKey[hashField.name] = hashField.import(
-            data.LastKeyEvaluated.HashKeyElement[hashField.shorType]
-        );
-
-        if(this.schema.range){
-            rangeField = this.schema.field(this.schema.range);
-            this.lastEvaluatedKey[rangeField.name] = rangeField.import(
-                data.LastKeyEvaluated.RangeKeyElement[rangeField.shorType]
-            );
-        }
-    }
-}
-
-// @todo (lucas) Get next result.
-ScanResult.prototype.next = function(){
-
-};
-
-// @todo (lucas) Get ALL results.
-ScanResult.prototype.all = function(){
-
+Model.prototype.scan = function(alias){
+    return new Scanner(this, alias);
 };
 
 
-
-Model.prototype.scan = function(alias, filter, opts){
+Model.prototype.runScan = function(alias, filter, opts){
     var self = this,
         schema = this.schema(alias),
         req = {
@@ -823,7 +717,7 @@ Model.prototype.scan = function(alias, filter, opts){
     }
 
     Object.keys(filter).forEach(function(key){
-        var f = new Filter(schema, key, filter[key]);
+        var f = new Scanner.Filter(schema, key, filter[key]);
         req.ScanFilter[key] = f.export();
 
     });
@@ -832,7 +726,7 @@ Model.prototype.scan = function(alias, filter, opts){
     // Make the request
     return this.db.scan(req).then(function(data){
         log.silly('SCAN returned: ' + util.inspect(data, false, 5));
-        return new ScanResult(self, alias, data);
+        return new Scanner.ScanResult(self, alias, data);
     }, function(err){
         log.error('SCAN: ' + err.message + '\n' + err.stack);
         return err;
---
+++ test/scan.test.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var assert = require('assert');
+
+var Scanner = require('../lib/scan'),
+    mambo = require('../'),
+    Schema = mambo.Schema,
+    StringField = mambo.StringField,
+    NumberField = mambo.NumberField,
+    JSONField = mambo.JSONField,
+    DateField = mambo.DateField,
+    StringSetField = mambo.StringSetField,
+    NumberSetField = mambo.NumberSetField;
+
+var songSchema = new Schema('Song', 'song', 'id', {
+    'id': NumberField,
+    'title': StringField,
+    'created': DateField,
+    'recent_loves': JSONField,
+    'tags': StringSetField,
+    'no_ones': NumberSetField
+});
+
+var loveSchema = new Schema('SongLoves', 'loves', ['id', 'created'], {
+    'id': NumberField,
+    'username': StringField,
+    'created': NumberField
+});
+
+var Song = new mambo.Model(songSchema, loveSchema);
+
+
+describe('Scan', function(){
+    describe('API', function(){
+        it('should return a query object when calling objects', function(){
+            var s = Song.scan('loves', 1);
+            assert.equal(s.constructor, Scanner);
+        });
+
+
+        it('should allow specifying a start key', function(){
+            var n = Date.now(),
+                s = Song.scan('loves').start({'id': 1, 'created': n});
+
+            assert.deepEqual(s._startKey, {'id': 1, 'created': n});
+        });
+
+        it('should allow specifying a limit', function(){
+            var q = Song.scan('loves').limit(5);
+            assert.equal(q._limit, 5);
+        });
+
+
+        it('should allow specifying fields', function(){
+            var s = Song.scan('loves').fields(['username', 'created']);
+            assert.deepEqual(s._fields, ['username', 'created']);
+        });
+    });
+});
